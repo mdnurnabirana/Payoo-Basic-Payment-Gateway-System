d741d1f1ea19ca092b5e8c177ebf86ad80216223
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-08-23 21:03:48 +0600

    Add home page and implement login functionality with validatin

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // Login Button Functionality
 document.getElementById("loginBtn").addEventListener("click", function(e){
     e.preventDefault();
-    console.log("Login Button Clicked!");
+    const mobileNumber = "01305592904";
+    const pinNumber = "1234";
+
+    const userInputMobileNumber = document.getElementById("mobile").value;
+    const userInputPin = document.getElementById("pin").value;
+
+    if(mobileNumber === userInputMobileNumber && pinNumber === userInputPin) {
+        window.location.href="./home.html";
+    } else {
+        window.alert("Invalid Credentials! Please Try Again!");
+    }
 });
