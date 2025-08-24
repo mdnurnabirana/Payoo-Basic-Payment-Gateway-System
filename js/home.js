9439a7893fa46a984f853b65397cd9b3fcb09568
Md. Nurnabi Rana <mdnurnabirana.cse@gmail.com>
2025-08-24 20:42:10 +0600

    Add cashout feature

--- js/home.js
+++ js/home.js
@@ -30,6 +30,8 @@ document.getElementById("addMoneyBtn").addEventListener("click", function(e){
 
     const totalBalance = amount + availableBalance;
     document.getElementById("available-balance").innerText = totalBalance;
+
+    alert("Amount added successfully!");
 })
 
 // All card IDs
@@ -90,4 +92,39 @@ cardIds.forEach(id => {
       }
     });
   }
+});
+
+// Cashout 
+document.getElementById("cashout-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const account = document.getElementById("cashout-account").value;
+    const amount = parseFloat(parseFloat(document.getElementById("cashout-amount").value).toFixed(2));
+    const pin = document.getElementById("cashout-pin").value;
+
+    if (account.length !== 11) {
+        alert("Your account number must be 11 digit!");
+        return;
+    }
+
+    if (amount < 10) {
+        alert("Amount must be 10 or above!");
+        return;
+    }
+
+    if (pin !== validPin) {
+        alert("Invalid Pin");
+        return;
+    }
+
+    const availableBalance = parseFloat(parseFloat(document.getElementById("available-balance").innerText).toFixed(2));
+
+    if (amount > availableBalance) {
+        alert("Insufficient balance!");
+        return;
+    }
+
+    const totalBalance = availableBalance - amount;
+    document.getElementById("available-balance").innerText = totalBalance;
+    alert("Cashout money successfull!");
 });
